6d8342e24f96c8d331782b120375e674de38a2d5
HARSHA REDDY <hvaitha07@gmail.com>
2024-03-20 01:57:23 -0400

    Data Ingestion

--- setup.py
+++ setup.py
@@ -1,30 +1,23 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
+import os
 
-HYPEN_E_DOT='-e.'
-
-def get_requirements(file_path:str)->List[str]:
-    '''
-    this function will retiurn the list of requirements
-    '''
-    requirements=[]
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function will return the list of requirements.
+    """
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+        requirements = [line.strip() for line in file_obj if line.strip() and not line.startswith('#')]
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-
-    return requirements        
-
-
+    # Print or log the requirements to debug
+    print("Parsed requirements:", requirements)
+    return requirements
 
 setup(
-name='mlproject',
-version='0.0.1',
-author='Harsha Vardhan',
-author_email='hvaitha07@gmail.com',
-packages=find_packages(),
-install_requires=get_requirements('requirements.txt')  
-
+    name='mlproject',
+    version='0.0.1',
+    author='Harsha Vardhan',
+    author_email='hvaitha07@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
 )

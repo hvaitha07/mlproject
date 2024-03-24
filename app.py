0242fc1238639dd18ec61032b21adc5cf0d4643d
HARSHA REDDY <hvaitha07@gmail.com>
2024-03-23 22:38:25 -0400

    Hyper parameter

--- app.py
+++ app.py
@@ -20,15 +20,15 @@ def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
     else:
-        data=CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
-
+        data = CustomData(
+    gender=request.form.get('gender'),
+    race_ethnicity=request.form.get('ethnicity'),
+    parental_level_of_education=request.form.get('parental_level_of_education'),
+    lunch=request.form.get('lunch'),
+    test_preparation_course=request.form.get('test_preparation_course'),
+    writing_score=float(request.form.get('writing_score')),  # Swapped to match variable names
+    reading_score=float(request.form.get('reading_score'))  # Swapped to match variable names
+        
         )
         pred_df=data.get_data_as_data_frame()
         print(pred_df)
@@ -42,5 +42,5 @@ def predict_datapoint():
     
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=8080)
 
